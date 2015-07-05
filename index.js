ede7a6830661cc464a0c5b13245084eb878821b9
xescuGC <xescugil@gmail.com>
2015-07-05 02:36:30 +0200

    Created basic tests for the first implementation

--- index.js
+++ index.js
@@ -1,39 +1,62 @@
-module.exports = function(format, cb, options) {
+module.exports = function(options, cb) {
 
-  options = options || {};
+  var format;
 
-  // Right not it only supports 'default' format
-  format = 'default';
+  if (typeof options === 'function') {
+    cb = options;
+    options = {};
+  };
 
   var extras = options.extras;
 
   var parent = options.parent;
 
+  var override = options.override;
+
   return function(req, res, next) {
     res.on('finish', function() {
-      var log = {
-        headers: req.headers,
-        url: req.originalUrl,
-        body: req.body,
-        query: req.query
-      }
+      var log = generateLog(options.format, req, res);
 
-      if (parent) {
-        var content = log; 
-        log = {};
-        log[parent] = content;
-      }
+      if (parent)
+        log = setParent(parent, log);
 
       if (extras) {
-        extras.foEach(function(c, i, a) {
-          Object.keys(c).forEach(function(_c, _i, _a) {
-            log[_c] = c[_c];
-          })
-        })
+        var extraFields = extras(req, res);
+        setExtras(extraFields, log);
+      }
+
+      if (override) {
+        log = {};
+        if (extras) {
+          var extraFields = extras(req, res);
+          setExtras(extraFields, log);
+        }
       }
 
       cb(log);
     })
     next();
+  }
+}
+
+function setParent(parent, log) {
+  var content = log; 
+  log = {};
+  log[parent] = content;
+  return log;
+}
+
+function setExtras(extras, log) {
+  Object.keys(extras).forEach(function(c, i, a) {
+    log[c] = extras[c];
+  })
+}
+
+function generateLog(format, req, res) {
+  return {
+    headers: req.headers,
+    url: req.originalUrl,
+    body: req.body,
+    query: req.query
   }
 }
---
+++ test/logson_test.js
@@ -0,0 +1,91 @@
+var logson = require('..');
+var chai = require('chai');
+var expect = chai.expect;
+var http = require('http');
+var httpMocks = require('node-mocks-http');
+var EventEmitter = require('events').EventEmitter;
+
+describe('Logson', function() {
+  describe('default', function() {
+    it('must return the default JSON', function(done) {
+      var req = httpMocks.createRequest();
+      var res = httpMocks.createResponse({
+          eventEmitter: EventEmitter
+      });
+      var result = logson(function(log) {
+        expect(log).to.eql({ headers: {}, url: '', body: {}, query: {} });
+        done();
+      })
+      result(req, res, nextFunction.bind(null, res));
+    })
+  })
+  describe('with options', function() {
+    it('with parent', function(done) {
+      var req = httpMocks.createRequest();
+      var res = httpMocks.createResponse({
+          eventEmitter: EventEmitter
+      });
+      var result = logson({parent: 'parent'}, function(log) {
+        expect(log).to.eql({parent: { headers: {}, url: '', body: {}, query: {} }});
+        done();
+      })
+      result(req, res, nextFunction.bind(null, res));
+    })
+    it('with extras', function(done) {
+      var req = httpMocks.createRequest();
+      var res = httpMocks.createResponse({
+          eventEmitter: EventEmitter
+      });
+      var options = {
+        extras: function(req, res) {
+          return {
+            cookies: req.cookies
+          }
+        }
+      }
+      var result = logson(options, function(log) {
+        expect(log).to.eql({ headers: {}, url: '', body: {}, query: {}, cookies: {}});
+        done();
+      })
+      result(req, res, nextFunction.bind(null, res));
+    })
+    describe('with override', function() {
+      it('default', function(done) {
+        var req = httpMocks.createRequest();
+        var res = httpMocks.createResponse({
+          eventEmitter: EventEmitter
+        });
+        var result = logson({ override: true }, function(log) {
+          expect(log).to.eql({});
+          done();
+        })
+        result(req, res, nextFunction.bind(null, res));
+      })
+      it('extras', function(done) {
+        var req = httpMocks.createRequest();
+        var res = httpMocks.createResponse({
+          eventEmitter: EventEmitter
+        });
+        var options = {
+          override: true,
+          extras: function(req, res) {
+            return {
+              cookies: req.cookies
+            }
+          }
+        }
+        var result = logson(options, function(log) {
+          expect(log).to.eql({cookies: {}});
+          done();
+        })
+        result(req, res, nextFunction.bind(null, res));
+      })
+    })
+  })
+})
+
+function nextFunction (res) {
+  res.end();
+  res.emit('finish');
+}
+
